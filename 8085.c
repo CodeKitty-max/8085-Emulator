7219e36d2944f70b8c7f5f45a388dad7116f8833
MeowCanCode <freddynjagi@gmail.com>
2025-08-27 13:33:06 +0300

    Function Declaration fix

--- 8085.c
+++ 8085.c
@@ -16,15 +16,82 @@
 typedef struct{
     //For 16-bit and 8-bit Operation Codes
     union{
-        __u16 (*operate)(void) = NULL;
-        __u8 (*operate)(void) = NULL;
-        void (*operate) (void) = NULL;
+        __u16 (*operate)(void);
+        __u8 (*operate)(void);
+        void (*operate) (void);
     } opcode;
-    __u8 (*addressMode)(void) = NULL;
-    __u8  cycles = 0;
+    __u8 (*addressMode)(void);
+    __u8  cycles;
 }instruction;
 
+//Sets flags as desired
+__u8 setFlags(bool Cval, bool Pval, bool Aval, bool Zval, bool Sval){
+    __u8 ret = 0;
+    __u8 flags[8] = {Cval, X, Pval, X, Aval, X, Zval, Sval};
+    for(__u8 i = 0; i < 8; i++){
+        if(flags[i])
+            ret |= (1 << i);
+    }       
+
+    return ret;
+}
+
+//Resets the flags
+__u8 resetFlags(__u8* flags){
+   //pointer to store the value of zero 
+    __u8* resValPointer;
+    __u8 resVal = 0;
+    resValPointer = &resVal;
+    
+   //reassigning value of flags to zero 
+    flags = resValPointer;
+    __u8 resetFlags = *(__u8*) flags;
+    return resetFlags;
+}
+
+
+//This is for getting the individual flags
+void getFlags(__u8* ret){
+    __u8 derefFlags = *(__u8*) ret;
+    __u8 flagArray[8];//8-bit array that contains 8-bit flag number
+    __u8 filteredArray[5];//filtered array that only contains 5 usable flags
+    //takes in decimal form of flags and converts them to binary
+    for(int i = 0; i < 8; i++){
+      flagArray[i] = derefFlags&(1<<i)?1:0;
+    }
+    //assigning values to the filtered array to form a 5 value array
+    filteredArray[0] = flagArray[0]; //Cval
+    filteredArray[1] = flagArray[2]; //Pval
+    filteredArray[2] = flagArray[4]; //Aval
+    filteredArray[3] = flagArray[6]; //Zval
+    filteredArray[4] = flagArray[7]; //Sval  
+
+    //create map for individual positions representing the various flags
+    insert("Cval", filteredArray[0]); //INDEX 0
+    insert("Pval", filteredArray[1]); //INDEX 1
+    insert("Aval", filteredArray[2]); //INDEX 2
+    insert("Zval", filteredArray[3]); //INDEX 3
+    insert("Sval", filteredArray[4]); //INDEX 4
+
+    //Printing the set flags
+    printSetFlagMap();
+}
+
+//Combining value in Accumulator with the flags in order to have a PSW value
+__u16 pswCombination(__u8 A, __u8 flags[8]){
+    __u16  PSW;
+    __u8 flagDecVal = 0; //storing the decimal value of the flag value
+
+    for (int i = 0; i < sizeof(flags)/sizeof(flags[i]); i++){
+        flagDecVal = (flagDecVal<<1) | flags[i];
+    }
+
+    PSW = (A << 8) | flagDecVal;//using the values in the Accumulator and Condition flag
+    return PSW;
+}
+
 //Assembly Instruction Lookup Table
+/*
 static const instruction[] ={
     ["SUB"] = {SUB, IMP, 7}, ["LXI"]  = {LXI , IMM, 3}, ["ANA"]  = {ANA, REG, 1}, ["HLT"] = {HLT, NULL, 1}, 
     ["ADD"] = {ADD, IMP, 2}, ["MOV"]  = {MOV , REG, 1}, ["ANI"]  = {ANI, IMM, 2}, ["NOP"] = {NOP, NULL, 0},
@@ -40,7 +107,7 @@ static const instruction[] ={
     ["DCX"] = {DCX, REG, 1}, ["PUSH"] = {PUSH, RIN, 3}, ["EI"]   = {EI , NULL, 1}, 
     ["DAD"] = {DAD, REG, 3}, ["SPHL"] = {SPHL, REG, 1}, ["DI"]   = {DI , NULL, 1},
 }; 
-
+*/
 
 
 int main(){
--- 8085.h
+++ 8085.h
@@ -33,73 +33,14 @@ __u16 stackPointer = 0x00;
 __u16 programCounter = 0x00;
 __u16 addressBusLatch = 0x00;
 
-//Sets flags as desired
-__u8 setFlags(bool Cval, bool Pval, bool Aval, bool Zval, bool Sval){
-    __u8 ret = 0;
-    __u8 flags[8] = {Cval, X, Pval, X, Aval, X, Zval, Sval};
-    for(__u8 i = 0; i < 8; i++){
-        if(flags[i])
-            ret |= (1 << i);
-    }       
 
-    return ret;
-}
-
-//Resets the flags
-__u8 resetFlags(__u8* flags){
-   //pointer to store the value of zero 
-    __u8* resValPointer;
-    __u8 resVal = 0;
-    resValPointer = &resVal;
-    
-   //reassigning value of flags to zero 
-    flags = resValPointer;
-    __u8 resetFlags = *(__u8*) flags;
-    return resetFlags;
-}
+//Function declarations
 
 
-//This is for getting the individual flags
-void getFlags(__u8* ret){
-    __u8 derefFlags = *(__u8*) ret;
-    __u8 flagArray[8];//8-bit array that contains 8-bit flag number
-    __u8 filteredArray[5];//filtered array that only contains 5 usable flags
-    //takes in decimal form of flags and converts them to binary
-    for(int i = 0; i < 8; i++){
-      flagArray[i] = derefFlags&(1<<i)?1:0;
-    }
-    //assigning values to the filtered array to form a 5 value array
-    filteredArray[0] = flagArray[0]; //Cval
-    filteredArray[1] = flagArray[2]; //Pval
-    filteredArray[2] = flagArray[4]; //Aval
-    filteredArray[3] = flagArray[6]; //Zval
-    filteredArray[4] = flagArray[7]; //Sval  
-
-    //create map for individual positions representing the various flags
-    insert("Cval", filteredArray[0]); //INDEX 0
-    insert("Pval", filteredArray[1]); //INDEX 1
-    insert("Aval", filteredArray[2]); //INDEX 2
-    insert("Zval", filteredArray[3]); //INDEX 3
-    insert("Sval", filteredArray[4]); //INDEX 4
-
-    //Printing the set flags
-    printSetFlagMap();
-}
-
-//Combining value in Accumulator with the flags in order to have a PSW value
-__u16 pswCombination(__u8 A, __u8 flags[8]){
-    __u16  PSW;
-    __u8 flagDecVal = 0; //storing the decimal value of the flag value
-
-    for (int i = 0; int < sizeof(flags)/sizeof(flags[i]); i++){
-        flagDecVal = (flagDecVal<<1) | flags[i];
-    }
-
-    PSW = (A << 8) | flagDecVal;//using the values in the Accumulator and Condition flag
-    return PSW;
-}
-
-
+__u8 setFlags(bool Cval, bool Pval, bool Aval, bool Zval, bool Sval);
+__u8 resetFlags(__u8* flags);
+void getFlags(__u8* ret);
+__u16 pswCombination(__u8 A, __u8 flags[8]);
 
 
 /*Addressing modes
@@ -122,10 +63,10 @@ __u16 CNZ(); void LHLD(); __u8 ORA(); void NOP();
 __u16 JNC(); void SHLD(); __u8 ORI(); void IN();
 __u16 ACI(); void LDAX(); __u8 XRA(); void OUT();
 __u16 SUI(); void XCHG(); __u8 XRI(); void POP();  
-__u16 SBB(); void XTHL(); __u8 CMP();
+__u16 SBB(); void XTHL(); __u8 CMP(); void PUSH();
 __u16 SBI(); void STAX(); __u8 CPI();
 __u16 INX(); void PCHL(); __u8 INR();         
-__u16 DCX(); void RST();   void SPHL();
-__u16 DAD(); void PUSH();
+__u16 DAD(); void RST();   void SPHL();
+
 
 
